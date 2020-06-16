1255a6847d54caefcb6146cd4ebc56affbc513f9
Ralph Robert Hossepian <ralphhovsepian@mail.ru>
2020-06-16 22:11:14 +0400

    Updates

---
+++ precache-manifest.6aac6f019788368d93b1122b65c45781.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ade1dd5186b386ed0e67a67ba73c86",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "493aaea50a0547460076",
+    "url": "/portfolio/static/css/main.dc624f1e.chunk.css"
+  },
+  {
+    "revision": "854e5539188195eff113",
+    "url": "/portfolio/static/js/2.aa8aae92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.aa8aae92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493aaea50a0547460076",
+    "url": "/portfolio/static/js/main.bf78c175.chunk.js"
+  },
+  {
+    "revision": "f0e7c031e4877f76c480",
+    "url": "/portfolio/static/js/runtime-main.a9e46812.js"
+  },
+  {
+    "revision": "257d995f70c43759ae1c5ec03bc5bc42",
+    "url": "/portfolio/static/media/40.257d995f.png"
+  },
+  {
+    "revision": "e2faec3c8515a8d41f55830cd9da7872",
+    "url": "/portfolio/static/media/45.e2faec3c.png"
+  },
+  {
+    "revision": "f7b74956627d5fb78298cd1c3d2ba398",
+    "url": "/portfolio/static/media/48.f7b74956.png"
+  },
+  {
+    "revision": "07883e93734b98cae0f7b9c55d287250",
+    "url": "/portfolio/static/media/github.07883e93.svg"
+  },
+  {
+    "revision": "04f1d8728b781908558c8f1442c665ed",
+    "url": "/portfolio/static/media/instagram.04f1d872.svg"
+  },
+  {
+    "revision": "007eb41ac7dadcaf97d565a787bf8afc",
+    "url": "/portfolio/static/media/linkedin.007eb41a.svg"
+  },
+  {
+    "revision": "a4f107de297f144be36b7831572a9793",
+    "url": "/portfolio/static/media/shortly.a4f107de.png"
+  },
+  {
+    "revision": "3a008e69b7247a6a8fe97ec45f0b11a6",
+    "url": "/portfolio/static/media/whereintheworld.3a008e69.png"
+  }
+]);
